852d7c4545f24ee73165a094b47f8313a176929e
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-09-24 17:28:24 -0400

    A reasonable Angband package

---
+++ modules/angband/manifests/deps.pp
@@ -0,0 +1,19 @@
+class angband::deps() {
+
+  package { [
+    "autoconf",
+    "gcc",
+    "libc6-dev",
+    # "libglade2-dev",
+    # "libgtk2.0-dev",
+    "libncurses5-dev",
+    "libncursesw5-dev",
+    # "libsdl1.2-dev",
+    # "libsdl-image1.2-dev",
+    # "libsdl-mixer1.2-dev",
+    # "libsdl-ttf2.0-dev",
+    # "libx11-dev",
+    "make"]:
+  }
+
+}
---
+++ modules/angband/manifests/init.pp
@@ -0,0 +1,33 @@
+class angband() {
+  $id = "angband-v3.5.0"
+  $filename = "${id}.tar.gz"
+  $url = "http://rephial.org/downloads/3.5/${filename}"
+  $install_dir = "/usr/local/games/${id}"
+  $target = "${install_dir}/games/angband"
+
+  include angband::deps
+
+  package { "wget":
+  }
+
+  exec { "download":
+    cwd => "/tmp",
+    command => "wget ${url}",
+    creates => "/tmp/${filename}",
+    require => Package["wget"],
+  }
+
+  exec { "unpack":
+    cwd => "/tmp",
+    command => "tar --extract --gzip --verbose --file ${filename}",
+    creates => "/tmp/${id}",
+    require => Exec["download"],
+  }
+
+  exec { "install":
+    cwd => "/tmp/${id}",
+    command => "bash -c './configure --prefix ${install_dir} && make && make install'",
+    creates => $install_dir,
+    require => [Class["Angband::Deps"], Exec["unpack"]],
+  }
+}

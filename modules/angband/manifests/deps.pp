08cc1d61139a7f7bb81470c48e0bdf1a3075914b
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-10 18:05:45 -0400

    So easy to have syntax errors in multiline arrays

--- modules/angband/manifests/deps.pp
+++ modules/angband/manifests/deps.pp
@@ -1,16 +1,16 @@
 class angband::deps() {
 
   package { [
+    # "libglade2-dev"
+    # "libgtk2.0-dev"
+    # "libsdl1.2-dev"
+    # "libsdl-image1.2-dev"
+    # "libsdl-mixer1.2-dev"
+    # "libsdl-ttf2.0-dev"
+    # "libx11-dev"
     "libc6-dev",
-    # "libglade2-dev",
-    # "libgtk2.0-dev",
     "libncurses5-dev",
-    "libncursesw5-dev",
-    # "libsdl1.2-dev",
-    # "libsdl-image1.2-dev",
-    # "libsdl-mixer1.2-dev",
-    # "libsdl-ttf2.0-dev",
-    # "libx11-dev"]:
+    "libncursesw5-dev"]:
   }
 
 }

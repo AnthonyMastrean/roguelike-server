681764d02ccac85e07affdaacb3b70a11ebd8159
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-21 17:16:44 -0400

    modules/adom

--- modules/adom/manifests/init.pp
+++ modules/adom/manifests/init.pp
@@ -1,28 +1,27 @@
 class adom() {
 	$url = "http://www.ancardia.com/download/adom_linux_ubuntu_32_1.2.0_pre23.tar.gz"
+
 	$version = "1.2.0_pre23"
-	$token = "${name}-${version}"
-	$install = "/usr/local/games/${token}"
-	$target = "${install}/adom"
+	$package = "${name}-${version}"
 
-	exec { "wget ${token}":
-		cwd => "/tmp",
-		command => "wget -O ${token}.tar.gz ${url}",
-		creates => "/tmp/${token}.tar.gz",
-		require => Package["wget"],
+	$archive = "/tmp/adom_linux_ubuntu_32_1.2.0_pre23.tar.gz"
+	$target = "/usr/local/games/adom"
+
+	include archive
+	include wget
+
+	wget::fetch { $package:
+		url => $url,
+		target => $archive,
 	}
 
-	exec { "extract ${token}":
-		cwd => "/tmp",
-		command => "tar --extract --gzip --verbose --file ${token}.tar.gz",
-		creates => "/tmp/adom",
-		require => Exec["wget ${token}"],
-	}
-
-	exec { "install ${token}":
-		command => "mv /tmp/adom ${install}",
-		creates => $install,
-		require => Exec["extract ${token}"],
+	archive::extract { $package:
+		source => $archive,
+		target => "/usr/local/games",
+		pathname => "adom/adom",
+		strip_path => 1,
+		creates => $target,
+		require => Wget::Fetch[$package],
 	}
 
 }

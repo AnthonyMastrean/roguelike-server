61e9f53957aef1903d3531275fbd1daef98c2ff5
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-23 16:35:55 -0400

    modules/brogue

--- modules/brogue/manifests/init.pp
+++ modules/brogue/manifests/init.pp
@@ -1,27 +1,42 @@
 class brogue() {
   $url = "https://sites.google.com/site/broguegame/brogue-1.7.4-linux-i386.tbz2"
+
   $version = "1.7.4"
-  $token = "${name}-${version}"
+  $package = "${name}-${version}"
 
-  exec { "wget ${token}":
-    cwd => "/tmp",
-    command => "wget -O ${token}.tar.gz ${url}",
-    creates => "/tmp/${token}.tar.gz",
-    require => Package["wget"],
+  $archive = "/tmp/brogue-1.7.4-linux-i386.tbz2"
+  $source = "/tmp/brogue-1.7.4"
+  $target = "/tmp/brogue-1.7.4/bin/brogue"
+  $link = "/usr/local/games/brogue"
+
+  include archive
+  include make
+  include wget
+
+  wget::fetch { $package:
+    url => $url,
+    target => $archive,
   }
 
-  exec { "extract ${token}":
-    cwd => "/tmp",
-    command => "tar --extract --gzip --verbose --file ${token}.tar.gz",
-    creates => "/tmp/brogue-1.7.4",
-    require => Exec["wget ${token}"],
+  archive::extract { $package:
+    source => $archive,
+    archive => bzip2,
+    target => "/tmp",
+    creates => $source,
+    require => Wget::Fetch[$package],
   }
 
-  exec { "install ${token}":
-    cwd => "/tmp/brogue-1.7.4",
-    command => "bash -c 'make curses'",
-    creates => $install,
-    require => Exec["extract ${token}"],
+  make::install { $package:
+    source => $source,
+    label => curses,
+    creates => $target,
+    require => Archive::Extract[$package],
+  }
+
+  file { $link:
+    ensure => link,
+    target => $target,
+    require => Make::Install[$package],
   }
   
 }

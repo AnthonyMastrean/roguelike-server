41211e510bb41383bfc29347d6b2820cdaf5116a
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-23 16:13:30 -0400

    modules/crawl

--- modules/crawl/manifests/init.pp
+++ modules/crawl/manifests/init.pp
@@ -1,20 +1,25 @@
 class crawl() {
   $version = "0.15"
-  $token = "${name}-${version}"
+  $package = "${name}-${version}"
 
-  file { "${token}.list":
+  exec { "${name} update":
+    path => ["/usr/bin"],
+    command => "apt-get update",
+  }
+
+  file { "${name}.list":
     path => "/etc/apt/sources.list.d/${name}.list",
     owner => root,
     group => root,
     mode => "a=r,u+w",
     content => "deb http://crawl.develz.org/debian crawl 0.15",
-    notify => Exec["apt-get update"],
   }
 
   exec { "wget crawl.develz.org":
-    command => "wget http://crawl.develz.org/debian/pubkey -O - | apt-key add -",
-    notify => Exec["apt-get update"],
+    path => ["/bin", "/usr/bin"],
+    command => "bash -c 'wget http://crawl.develz.org/debian/pubkey -O - | apt-key add -'",
     require => File["${name}.list"],
+    notify => Exec["${name} update"],
   }
 
   package { "crawl":

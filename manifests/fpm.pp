ec9659927b5e2c8f6d082acd42acb922f63d08c7
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-11-07 16:48:02 -0500

    damn, if I never figure out system package deps

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -6,19 +6,8 @@ Exec["apt-get update"] -> Package<| |>
 
 package { [
   "gcc", 
-  "make", 
-  "rpm",
   "tar",
   "wget"]:
-}
-
-file { ".gemrc":
-  path => "/home/vagrant/.gemrc",
-  content => "gem: --no-document\ngem: --no-rdoc --no-ri\n",
-}
-
-package { "ruby-dev":
-  require => [File[".gemrc"], Package["make"]],
 }
 
 include fpm
--- modules/fpm/manifests/init.pp
+++ modules/fpm/manifests/init.pp
@@ -1,7 +1,11 @@
 class fpm() {
 
+  package { ["ruby-dev", "rpm", "make"]:
+  }
+
   exec { "gem install fpm":
     path => ["/opt/vagrant_ruby/bin"],
+    command => "gem install fpm --no-rdoc --no-ri",
     require => Package["ruby-dev"],
   }
 
